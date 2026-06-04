fe708adacefb390fcf6d16def7939b9901bae389
Vishal <vishalkumarmalladi@gmail.com>
2026-06-04 11:28:35 +0530

    Created threads and performed parallel computation

--- fuzzs.cpp
+++ fuzzs.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int minDistance(string word1, string word2) {
+int mindistance(string word1, string word2) {
     int m= word1.size(), n= word2.size();
     vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
     for(int j=0; j<n; j++) dp[m][j] = n-j;
@@ -21,11 +21,20 @@ int minDistance(string word1, string word2) {
     }
     return dp[0][0];
 }
+
+//Each thread will be processing a chunk of the dictionary.
+
+void processchunk(const vector<string> &words, const string &query, int start, int end, vector<pair<int, string>> &localresults){
+    for(int i=start; i<end; i++){
+        int dist = mindistance(query, words[i]);
+        localresults.emplace_back(dist, words[i]);
+    }
+}
 int main(int argc, char* argv[]){
-    //I expect the input to be in the form "command query dictionaryfilename.txt".
+    //I expect the input to be in the form "<command> <query> <dictionary_filename>".
     //Since there are 3 parts to it, argc should be 3.
     if(argc != 3){
-        cout<<"Usage: "<<argv[0]<<"<query> <dictionary_filename>"<<endl;
+        cout<<"Usage: "<<argv[0]<<" <query> <dictionary_filename>"<<endl;
         return 1;
     }
     string query= argv[1];
@@ -35,5 +44,29 @@ int main(int argc, char* argv[]){
         cout<<"Error opening file: "<<filename<<'\n';
         return 1;
     }
+    vector<string> words;
+    string word;
+    while(fin >> word){
+        words.push_back(word);
+    }
+    if(words.empty()){
+        cout<<"The dictionary file is empty"<<'\n';
+        return 1;
+    }
+    //Lets fix number of threads as 4.
+    int n= words.size();
+    int chunksize = (n+3)/4;
+    vector<pair<int, string>> results1;
+    vector<pair<int, string>> results2;
+    vector<pair<int, string>> results3;
+    vector<pair<int, string>> results4;
+    thread t1(processchunk, cref(words), cref(query), 0, chunksize, ref(results1));
+    thread t2(processchunk, cref(words), cref(query), chunksize, 2*chunksize, ref(results2));
+    thread t3(processchunk, cref(words), cref(query), 2*chunksize, 3*chunksize, ref(results3));
+    thread t4(processchunk, cref(words), cref(query), 3*chunksize, n, ref(results4));
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
     return 0;
 }
